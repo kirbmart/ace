a26661832e0c853ffbc364d0da4053dfe7a83207
kirbmart <brady@goodlunch.com>
2014-06-08 15:03:52 -0600

    name to user, name validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608195834) do
+ActiveRecord::Schema.define(version: 20140608204306) do
 
   create_table "games", force: true do |t|
     t.datetime "created_at"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140608195834) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
