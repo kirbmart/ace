a7d0a848fba0d106933773076593e0e162de667d
kirbmart <brady@goodlunch.com>
2014-06-08 14:42:05 -0600

    added games resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607053913) do
+ActiveRecord::Schema.define(version: 20140608195834) do
+
+  create_table "games", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
